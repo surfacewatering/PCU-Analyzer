ebc9ab4b99d6d4cf616fc163b6228ed01bf6b29e
OREO1210 <bittoo979@gmail.com>
2023-02-09 18:31:13 +0530

    updates

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI
+from pydantic import BaseModel
 from fastapi import FastAPI, Request, Response, status
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Form, Cookie, Depends
+from starlette.responses import RedirectResponse, Response
+import datetime
+import time
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
