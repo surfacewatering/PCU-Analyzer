900a6ae48e4d54898f610b61072806fc1d06fc17
OREO1210 <bittoo979@gmail.com>
2023-02-13 18:35:32 +0530

    files added

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from fastapi import FastAPI, Form, Cookie, Depends
 from starlette.responses import RedirectResponse, Response
 import datetime
 import time
+import csv
+from fastapi import FastAPI, File, UploadFile
+import pandas as pd
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
@@ -14,3 +17,14 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 def home_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/pcu", response_class=HTMLResponse)
+def find_pcu(request: Request, fileName:str= Form(...)):
+    #initialize the model
+    return templates.TemplateResponse("list_fish.html", {"request": request})
+
+
+@app.post("/")
+def upload(request: Request, file: UploadFile = File(...)):
+    print(file.filename)
+    return templates.TemplateResponse('results.html', context={'request': request})
